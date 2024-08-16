73bf91bbbf003ae79d360ad7521c7aef7aa284a6
robinrolle <43077428+robinrolle@users.noreply.github.com>
2024-08-16 15:29:49 +0200

    Add debounce for SearchBar

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,9 +1,10 @@
 // components/SearchBar.js
 
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { styled } from '@mui/material/styles';
+import debounce from 'lodash.debounce';
 
 const CustomTextField = styled(TextField)({
     '& .MuiOutlinedInput-root': {
@@ -12,12 +13,31 @@ const CustomTextField = styled(TextField)({
 });
 
 const SearchBar = ({ options, value, onChange, placeholder }) => {
+    const [inputValue, setInputValue] = useState(value);
+
+    const debouncedOnChange = useCallback(
+        debounce((newValue) => {
+            onChange(newValue);
+        }, 300), // Adjust the debounce delay (in milliseconds) as needed
+        []
+    );
+
+    useEffect(() => {
+        debouncedOnChange(inputValue);
+        // Cleanup function to cancel any pending debounced calls when the component unmounts
+        return () => {
+            debouncedOnChange.cancel();
+        };
+    }, [inputValue, debouncedOnChange]);
+
     return (
         <Autocomplete
             multiple
             options={options}
             value={value}
-            onChange={(event, newValue) => onChange(newValue)}
+            onChange={(event, newValue) => {
+                setInputValue(newValue);
+            }}
             renderTags={() => null} // Pour masquer les Chips
             renderInput={(params) => (
                 <CustomTextField
