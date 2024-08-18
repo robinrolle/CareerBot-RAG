e5d89719338c456fd7d455c38b186b8f994cd6ac
robinrolle <43077428+robinrolle@users.noreply.github.com>
2024-08-18 08:54:19 +0200

    Add similar item search for suggestions

--- POC/retrieval/basic_rag.py
+++ POC/retrieval/basic_rag.py
@@ -2,6 +2,7 @@ import os
 import re
 import numpy as np
 import pandas as pd
+
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain.prompts import ChatPromptTemplate
@@ -118,38 +119,115 @@ def create_item_relevance_mapping(answer):
 
     return item_relevance_map
 
+def structure_terms(retrieved_docs, response_processed, doc_type):
+    structured_docs = []
+
+    for i, doc in enumerate(retrieved_docs['documents'][0]):
+        for term in response_processed.keys():
+            labels = doc.split('|')[0]
+            if term in labels:
+                relevance = response_processed[term]
+                if isinstance(relevance, str):
+                    relevance_mapping = {'HIGH': 3, 'MEDIUM': 2, 'LOW': 1}
+                    relevance = relevance_mapping[relevance]
+
+                structured_docs.append({
+                    'embedding': retrieved_docs['embeddings'][0][i],
+                    'document': doc,
+                    'relevance': relevance,
+                    'type': doc_type,
+                    'label': labels.strip()
+                })
+                break
+
+    return structured_docs
+
+def get_similar_documents(db, docs, doc_type, n_suggest):
+    # Memorise already selected docs
+    used_docs = set([x['document'] for x in docs])
+    n_old = len(used_docs)
+    n_new = 2 * int(np.ceil(n_suggest / n_old))
+    new_docs = []
+
+    # Get new similar documents
+    for current_doc in docs:
+        suggested_docs = db._collection.query(
+            query_embeddings=[current_doc['embedding']],
+            n_results=n_old + n_new,
+            include=['embeddings', 'documents', 'metadatas'],
+            where={"type": doc_type}
+        )
+        z = suggested_docs['documents'][0]
+        cands = [{'document': z[k], 'embedding': suggested_docs['embeddings'][0][k]} for k in range(len(z)) if (z[k] not in used_docs)]
+        new_docs.append(cands)
+        used_docs.update([x['document'] for x in cands])
+
+    # Sélectionner les suggestions finales
+    round = -1
+    final_docs = []
+
+    while True:
+        round += 1
+        is_empty = True
+        for cands in new_docs:
+            if len(cands) > round:
+                is_empty = False
+                if len(final_docs) < n_suggest:
+                    final_docs.append(cands[round])
+                else:
+                    break
+        if is_empty or len(final_docs) == n_suggest:
+            break
+
+    # Format final result
+    for k, x in enumerate(final_docs):
+        final_docs[k]['label'] = x['document'].split('|')[0].strip()
+
+    return final_docs
+
+
 if __name__ == "__main__":
+    # Init embedding functiion
     embedding_ef = OpenAIEmbeddings(model=EMBEDDING_MODEL_NAME)
 
     collection_name = generate_valid_collection_name(EMBEDDING_MODEL_NAME)
-    chroma_db = init_chroma_db(collection_name, embedding_ef)
+    chroma_client = chromadb.PersistentClient(path=str(DATABASE_DIR))
+    db = Chroma(client=chroma_client, collection_name=collection_name, embedding_function=embedding_ef)
 
-    skills_retriever = get_retriever(db=chroma_db, max_retrieved=VECTORSTORE_MAX_SUGGESTED[0], doc_type="skill/competence")
-    occupation_retriever = get_retriever(db=chroma_db, max_retrieved=VECTORSTORE_MAX_SUGGESTED[1], doc_type="occupation")
-
-    # Example of user input text
     input_text = """
-    I have worked last 5 years as an contractor that sells and installs heat pumps for consumers. 
-    I have engineering degree and know lots of refrigerants, and technology of heating and cooling systems. 
-    I have licence to do electrical jobs."""
+    I have worked last 5 years as a contractor that sells and installs heat pumps for consumers. 
+    I have an engineering degree and know lots of refrigerants, and technology of heating and cooling systems. 
+    I have a license to do electrical jobs."""
 
-    selected_skills = skills_retriever.invoke(input=input_text)
-    selected_occupations = occupation_retriever.invoke(input=input_text)
+    # Turn query into embedding
+    input_embedding = embedding_ef.embed_query(text=input_text)
 
-    context_skills = format_skills_for_prompt(selected_skills)
-    context_occupations = format_occupations_for_prompt(selected_occupations)
+    # Retrieve documents by querying the collection
+    retrieved_skills = db._collection.query(query_embeddings=input_embedding, n_results=VECTORSTORE_MAX_RETRIEVED,include=['embeddings','documents'],where={"type": "skill/competence"})
+    retrieved_occupations = db._collection.query(query_embeddings=input_embedding, n_results=VECTORSTORE_MAX_RETRIEVED, include=['embeddings', 'documents'], where={"type": "occupation"})
 
-    # Initialize the LLM chain
+    # Output the embeddings
+    print("Skills Embeddings:", retrieved_skills['documents'][0])
+    print("Occupation Embeddings:", retrieved_occupations['documents'][0])
+
+    # Clean and prepare the contexts for the prompt
+    context_skills = [x.replace('\n', ' ').replace('\r', '') for x in retrieved_skills['documents'][0]]
+    context_skills = [x for x in context_skills if len(x) > 4]  # Only include documents with more than 4 characters
+    context_skills = '\n\n'.join(context_skills)
+    context_occupations = [x.replace('\n', ' ').replace('\r', '') for x in retrieved_occupations['documents'][0]]
+    context_occupations = [x for x in context_occupations if len(x) > 4]  # Only include documents with more than 4 characters
+    context_occupations = '\n\n'.join(context_occupations)
+
     chain = initialize_chain()
 
-    # Generate the final response
+    # LLM call for skills
     skills_response = chain.invoke({
         "question": input_text,
         "context": context_skills,
         "LLM_MAX_PICKS": LLM_MAX_PICKS[0],
     })
 
-    # Generate the final response
+    # LLM call for occupations
     occupations_response = chain.invoke({
         "question": input_text,
         "context": context_occupations,
@@ -157,10 +235,32 @@ if __name__ == "__main__":
     })
 
     print("Generated Response:")
-    print(f"occupation {occupations_response}")
-    print(f"skills {skills_response}")
+    print(f"Skills: {skills_response}")
+    print(f"Occupations: {occupations_response}")
 
     response_processed_skills = create_item_relevance_mapping(skills_response)
     response_processed_occupation = create_item_relevance_mapping(occupations_response)
-    print(f"skills mapping : {create_item_relevance_mapping(skills_response)}")
-    print(f"occupations mapping : {create_item_relevance_mapping(occupations_response)}")
+
+    print(f"Skills mapping: {response_processed_skills}")
+    print(f"Occupations mapping: {response_processed_occupation}")
+
+    # Structure docs
+    structured_skills = structure_terms(retrieved_skills, response_processed_skills, 'skill/competence')
+    print("Structured Skills:", structured_skills)
+    structured_occupations = structure_terms(retrieved_occupations, response_processed_occupation, 'occupation')
+    print("Structured Occupations:", structured_occupations)
+
+    # Similarity search for suggestions
+    similar_skills = get_similar_documents(db, structured_skills, 'skill/competence', VECTORSTORE_MAX_SUGGESTED[0])
+    similar_occupations = get_similar_documents(db, structured_occupations, 'occupation', VECTORSTORE_MAX_SUGGESTED[1])
+
+
+    # Parse and display only the labels
+    print("Labels for Similar Skills Suggestions:")
+    for doc in similar_skills:
+        print(doc['label'])
+
+    print("Labels for Similar Occupations Suggestions:")
+    for doc in similar_occupations:
+        print(doc['label'])
+
