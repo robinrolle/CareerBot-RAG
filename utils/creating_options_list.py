8cfd4f9628c0d3009e4040788501d9a8857cc1fd
robinrolle <robinrolle1748@gmail.com>
2024-09-18 23:51:30 +0200

    Finalize repo, ensure dependencies compatibility

--- utils/creating_options_list.py
+++ utils/creating_options_list.py
@@ -1,6 +1,11 @@
-import os
 import json
 import re
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
 from Backend.app.config import EMBEDDING_MODEL_NAME
 
 # Define paths
